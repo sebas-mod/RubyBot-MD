f3583be4eeae472628c0dde5f7f81c670b17d233
Karim-off <153532713+Karim-off@users.noreply.github.com>
2024-04-28 11:44:43 -0400

    Actualizar nv-global.js

--- plugins/nv-global.js
+++ plugins/nv-global.js
@@ -379,7 +379,7 @@ handler.all = async function(m, {conn}) {
   if (!chat.isBanned && m.text.match(/(turip)/gi)) {
     if (!db.data.chats[m.chat].audios) return;
     if (!db.data.settings[mconn.conn.user.jid].audios_bot && !m.isGroup) return;
-    const vn = './media/turi.mp3';
+    const vn = './media/turip.mp3';
     mconn.conn.sendPresenceUpdate('recording', m.chat);
     mconn.conn.sendMessage(m.chat, {audio: {url: vn}, fileName: 'error.mp3', mimetype: 'audio/mpeg', ptt: true}, {quoted: m});
   }
