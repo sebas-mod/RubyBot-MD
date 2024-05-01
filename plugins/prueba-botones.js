866154139448549dae2a8de27a5c6a47d6934e6b
Karim-off <153532713+Karim-off@users.noreply.github.com>
2024-05-01 15:19:34 -0400

    Actualizar prueba-botones.js

--- plugins/prueba-botones.js
+++ plugins/prueba-botones.js
@@ -23,22 +23,42 @@ let msg = generateWAMessageFromContent(m.chat, {
             hasMediaAttachment: false
           }),
           nativeFlowMessage: proto.Message.InteractiveMessage.NativeFlowMessage.create({
-            buttons:  
+            buttons: [
               {
-                "name": "quick_Menu",
+                "name": "single_select",
+                "buttonParamsJson": "{\"title\":\"title\",\"sections\":[{\"title\":\"title\",\"highlight_label\":\"label\",\"rows\":[{\"header\":\"header\",\"title\":\"title\",\"description\":\"description\",\"id\":\"id\"},{\"header\":\"header\",\"title\":\"title\",\"description\":\"description\",\"id\":\"id\"}]}]}"
+              },
+              {
+                "name": "quick_reply",
                 "buttonParamsJson": "{\"display_text\":\".menu\",\"id\":\"message\"}"
               },
               {
-                 "name": "cta_grupo",
-                 "buttonParamsJson": "{\"display_text\":\"grupo\",\"grupo\":\"https://chat.whatsapp.com/LcFTUnvu0Tw1tCnA2ybdR6\",\"merchant_url\":\"https://chat.whatsapp.com/LcFTUnvu0Tw1tCnA2ybdR6"}"
+                 "name": "cta_url",
+                 "buttonParamsJson": "{\"display_text\":\"url\",\"url\":\"https://www.google.com\",\"merchant_url\":\"https://www.google.com\"}"
               },
               {
-                 "name": "cta_llamar",
+                 "name": "cta_call",
                  "buttonParamsJson": "{\"display_text\":\"call\",\"id\":\"message\"}"
               },
               {
                  "name": "cta_copy",
                  "buttonParamsJson": "{\"display_text\":\"copy\",\"id\":\"123456789\",\"copy_code\":\"message\"}"
+              },
+              {
+                 "name": "cta_reminder",
+                 "buttonParamsJson": "{\"display_text\":\"cta_reminder\",\"id\":\"message\"}"
+              },
+              {
+                 "name": "cta_cancel_reminder",
+                 "buttonParamsJson": "{\"display_text\":\"cta_cancel_reminder\",\"id\":\"message\"}"
+              },
+              {
+                 "name": "address_message",
+                 "buttonParamsJson": "{\"display_text\":\"address_message\",\"id\":\"message\"}"
+              },
+              {
+                 "name": "send_location",
+                 "buttonParamsJson": ""
               }
            ],
           })
@@ -52,4 +72,4 @@ await conn.relayMessage(msg.key.remoteJid, msg.message, { messageId: msg.key.id 
 }
 handler.command = /^(mboton)$/i
 
-export default handler  
+export default handler
