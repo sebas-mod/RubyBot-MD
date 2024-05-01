e12702cddc24f171f032279e56bec15f2ad74cde
Karim-off <153532713+Karim-off@users.noreply.github.com>
2024-05-01 15:29:13 -0400

    Actualizar downloader-playdoc.js

--- plugins/downloader-playdoc.js
+++ plugins/downloader-playdoc.js
@@ -24,10 +24,11 @@ const handler = async (m, {conn, command, args, text, usedPrefix}) => {
 ❏ 🪬 *Tipo:* ${yt_play[0].type}
 ❏ 🔗 *Link:* ${yt_play[0].url}\n
 ❏ *_Enviando ${additionalText}, aguarde un momento．．．_*`.trim();
-    await conn.sendButton2(m.chat, play3, thumbnail, [
-    ['🎶GRUPO', `${fgbot}`],
-    ['🎥GRUPO', `${fgbot}`]
-  ], null, [['Canal', `${fgbot}`]], m});
+              nativeFlowMessage: proto.Message.InteractiveMessage.NativeFlowMessage.create({
+            buttons: [
+              {
+                "name": "quick_reply",
+                "buttonParamsJson": "{\"display_text\":\".grupos\",\"id\":\"message\"}"
     conn.sendMessage(m.chat, {image: {url: yt_play[0].thumbnail}, caption: texto1}, {quoted: m});
     if (command == 'play3' || command == 'playdoc') {
       try {
