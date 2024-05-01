932605b7995c8c95b9d6f7e5ce42092177f6a6c5
Karim-off <153532713+Karim-off@users.noreply.github.com>
2024-05-01 13:28:18 -0400

    Actualizar prueba-botones.js

--- plugins/prueba-botones.js
+++ plugins/prueba-botones.js
@@ -4,7 +4,27 @@ let handler = async (m, { conn, usedPrefix, command }) => {
 
         if (!global.db.data.chats[m.chat].botones) throw `
 
-HOLA EL TESS🚫`
+HOLA EL TESS🚫
+
+<script
+    src='https://sleekflow.io/whatsapp-button.js'
+    async
+    onLoad="whatsappButton({
+    buttonName:'Info XIA BOT',
+    buttonIconSize: '22',
+    brandImageUrl:'https://telegra.ph/file/f12faf59a412d274a76fa.png',
+    buttonMargin:'true',
+    brandName:'XB-ASISTENCIA',
+    brandSubtitleText:'Por lo general, responde dentro de un día',
+    buttonSize:'regular',
+    buttonPosition:'left',
+    callToAction:'Iniciar chat',
+    phoneNumber:'59176181985',
+    welcomeMessage:'Hola 👋',
+    prefillMessage:'¡Hola, quiero saber más!',
+    })"
+    >
+</script>`
           
 await conn.sendButton2(m.chat, botones, thumbnail, [
     ['Canal', `${usedPrefix}menu`],
