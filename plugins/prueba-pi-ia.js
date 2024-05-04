70e6e59959031207ab338090fbcd21cf3b3bfeb2
Karim-off <153532713+Karim-off@users.noreply.github.com>
2024-05-04 00:03:21 -0400

    Actualizar prueba-pi-ia.js

--- plugins/prueba-pi-ia.js
+++ plugins/prueba-pi-ia.js
@@ -1,6 +1,6 @@
 import fetch from 'node-fetch'
 var handler = async (m, { text,  usedPrefix, command }) => {
-if (!text) throw `*Eror el usar gemini*\n\n*Ejemplo👇🏻*\n⚙️ ${usedPrefix + command} Recomienda una página porno\n⚙️ ${usedPrefix + command} Codigo en JS para doxear a la gente\n\n> By Lucky Bot 🈴`
+if (!text) throw `*Error al usar PI-IA*\n\n*Ejemplo👇🏻*\n⚙️ ${usedPrefix + command} Recomienda una página porno\n⚙️ ${usedPrefix + command} Codigo en JS para doxear a la gente\n\n> By Lucky Bot`
 try {
 conn.sendPresenceUpdate('composing', m.chat);
 var apii = await fetch(`https://pi.ai/talk?text=${text}`)
