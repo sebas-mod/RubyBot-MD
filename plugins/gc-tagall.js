4c7c42759e39b84ec25f8d19e2928541e6310daa
Karim-off <153532713+Karim-off@users.noreply.github.com>
2024-05-05 15:57:18 -0400

    Actualizar gc-tagall.js

--- plugins/gc-tagall.js
+++ plugins/gc-tagall.js
@@ -6,11 +6,11 @@ const handler = async (m, {isOwner, isAdmin, conn, text, participants, args, com
   }
   const pesan = args.join` `;
   const oi = `*𝙼𝙴𝙽𝚂𝙰𝙹𝙴:* ${pesan}`;
-  let teks = `*⺀𝚁 𝙴 𝚅 𝙸 𝚅 𝙰 𝙽 - 𝙱 𝙾 𝚃 𝚂⺀*\n\n❏ ${oi}\n\n❏ *𝙴𝚃𝙸𝚀𝚄𝙴𝚃𝙰𝚂:*\n`;
+  let teks = `*⺀𝚁 𝙴 𝚅 𝙸 𝚅 𝙰 𝙽 - 𝙱 𝙾 𝚃 𝚂⺀*\n\n❏ ${oi}\n\n❏┏・ *𝙴𝚃𝙸𝚀𝚄𝙴𝚃𝙰𝚂:*\n`;
   for (const mem of participants) {
-    teks += `┣・🐲 @${mem.id.split('@')[0]}\n`;
+    teks += `┣・💜 @${mem.id.split('@')[0]}\n`;
   }
-  teks += `┗・🐉 𝑳𝒖𝒄𝒌𝒚𝑩𝒐𝒕-𝑴𝑫\n\n`;
+  teks += `┗・💜 @𝙍𝙪𝙗𝙮𝘽𝙤𝙩-𝙈𝘿\n\n`;
   conn.sendMessage(m.chat, {text: teks, mentions: participants.map((a) => a.id)} );
 };
 handler.help = ['tagall <mesaje>', 'invocar <mesaje>'];
